7b146a9d58019344911edfd99ef6c75969a1941c
Jordy Murgueitio <jordinho.15@gmail.com>
2025-08-11 17:44:05 -0500

    improved about me section

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,19 @@ function App() {
       </header>
       <section id='about-section'>
         <div className='about-text-container'>        
-          <h2>Hi There!</h2>
-          <p>Welcome to my portfolio! I'm Jordy, a passionate developer with experience in various technologies.</p>
+          <h2>Hi,</h2>
+          <h2>I'm Jordy,</h2>
+          <h2>Front-end developer</h2>
+          <p>
+            Passionate about building responsive, user-friendly websites and apps. 
+            Skilled in HTML, CSS, JavaScript, React, and SQL, 
+            I focus on creating smooth, functional experiences that look great and work seamlessly. 
+            I enjoy solving problems, refining details, and bringing ideas to life through clean, efficient code.
+          </p>
+          <div className='buttons-container'>
+            <a href="#contact">Contact Me</a>
+            <a href="#projects">View Projects</a>
+          </div>
         </div>
         <img src={jordyImg} className='jordy-image' alt="Jordy Image" />
       </section>
