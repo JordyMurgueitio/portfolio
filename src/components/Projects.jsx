d53d853a5175147404b015f31cb309e6c5e779b6
Jordy Murgueitio <jordinho.15@gmail.com>
2025-08-12 17:05:09 -0500

    projects section finished first version

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,23 @@
+import weather from '../assets/weather-app.png'
+import filmFinder from '../assets/film-app.png'
+
 function Projects() {
     return (
-        <section id='project-section'>
+        <section id='projects-section'>
             <div className="projects-header">
                 <h2>Projects</h2>
                 <p>Here are some of my recent projects:</p>
             </div>
             <div className="projects-container">
                 <div className="project-box">
-                    <img className="project-image" src="" alt="" />
+                    <img className="project-image" src={filmFinder} alt="Film Finder" />
                     <h5 className="project-title">Film Finder</h5>
-                    <a className="project-link" href="./projects/film-finder/index.html" target="_blank">Open project</a>
+                    <a className="project-link" href="https://jordymurgueitio.github.io/film-finder-app/" target="_blank">Open project</a>
                 </div>
                 <div className="project-box">
-                    <img className="project-image" src="" alt="" />
+                    <img className="project-image" src={weather} alt="Weather App" />
                     <h5 className="project-title">Weather App</h5>
-                    <a className="project-link" href="./projects/weather-app/index.html" target="_blank">Open project</a>
+                    <a className="project-link" href="https://jordymurgueitio.github.io/weather-app/" target="_blank">Open project</a>
                 </div>
             </div>
     </section>
